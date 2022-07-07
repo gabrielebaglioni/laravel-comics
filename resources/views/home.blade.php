5aaa8b60ff8231a23875a71e9808b38bfd42fc73
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-07-07 17:20:28 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.base')
+
+  @section('page-title')
+      Home page
+  @endsection
+  @section('page-content')
+      Contenuto
+  @endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+   <header class="header">
+      <div class="header__logo">
+        <img src="../assets/img/img/dc-logo.png" alt="">
+      </div>
+      <nav class="header__nav">
+        <ul class="header__list"> 
+          <li><a href="#"></a>CHARACTERS</li>
+          <li><a href="#"></a>COMICS</li>
+          <li><a href="#"></a>MOVIES</li>
+          <li><a href="#"></a>TV</li>
+          <li><a href="#"></a>GAMES</li>
+          <li><a href="#"></a>COLLECTIBLES</li>
+          <li><a href="#"></a>VIDEOS</li>
+          <li><a href="#"></a>FANS</li>
+          <li><a href="#"></a>NEWS</li>
+          <li><a href="#"></a>SHOP</li>
+        </ul>
+      </nav>
+      
+   </header>
+</header>
