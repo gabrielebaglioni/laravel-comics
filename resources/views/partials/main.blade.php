94c5a93cee0b50cf3aa7b75bdae49b1341953816
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-07-08 18:05:21 +0200

    main fatto

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,3 +1,18 @@
 <main>
    <section class="jumbotron"></section>
+   <section class="bg-[#1C1C1C] p-3">
+      <div class="text-center relative container">
+         <button class=" btn bg-[#0082F9] text-white p-1">Current Series</button>
+         <div class="flex flex-wrap px-6 w-full py-8">
+          @foreach ($comics as $comic)
+            <div class="px-6 py-4 w-1/6 card">
+               <img src="{{asset($comic["thumb"])}}" alt="{{$comic["series"]}}">
+               <small class="text-white mt-2 uppercase">{{$comic["series"]}}</small>
+            </div>
+           @endforeach  
+         </div>
+
+      </div>
+        
+   </section>
 </main>
